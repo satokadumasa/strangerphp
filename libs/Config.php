de0b0233fe424a90155477d08512228e0fce5d60
佐藤憲匡 <satokadumasa@satoukentadashi-no-MacBook-Air.local>
2017-06-01 13:41:09 +0900

    環境変数によりConfigの設定ファイル読み込み先を変更するように修正

--- libs/Config.php
+++ libs/Config.php
@@ -1,7 +1,7 @@
 <?php
 class Config {
   public static function get($config) {
-    require_once CONFIG_PATH.$config.".php";
+    require_once CONFIG_PATH.getenv('ENVIRONMENT')."/".$config.".php";
     $vars = get_defined_vars();
     return $vars;
   }
