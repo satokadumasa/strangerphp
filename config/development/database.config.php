7f25ee78b1cbc136ef02b4b28381a114eedec4fe
佐藤憲匡 <satokadumasa@satoukentadashi-no-MacBook-Air.local>
2017-06-08 19:36:08 +0900

    動作環境設定を修正

--- config/development/database.config.php
+++ config/development/database.config.php
@@ -7,21 +7,3 @@ $default_database = array(
   'username' => 'root',
   'password' => 'glxre297',
 );
-
-$slave1 = array(
-  'rdb'      => 'mysql',
-  'host'     => 'localhost',
-  'dbname'   => 'cinnamon',
-  'charset'  => 'utf8',
-  'username' => 'root',
-  'password' => 'glxre297',
-);
-
-$slave2 = array(
-  'rdb'      => 'mysql',
-  'host'     => 'localhost',
-  'dbname'   => 'cinnamon',
-  'charset'  => 'utf8',
-  'username' => 'root',
-  'password' => 'glxre297',
-);
