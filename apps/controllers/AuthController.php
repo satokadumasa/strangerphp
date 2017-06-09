a756b7d112925eafefac360b572005b4082e5cd4
佐藤憲匡 <satokadumasa@satoukentadashi-no-MacBook-Air.local>
2017-06-09 14:44:55 +0900

    メール送信のバッチ化（８）

--- apps/controllers/AuthController.php
+++ apps/controllers/AuthController.php
@@ -97,7 +97,6 @@ class AuthController extends BaseController{
       $cmd = 'php ' . BIN_PATH . 'send_notify.php';
       $this->debug->log("AuthController::save() exec_cmd:".$cmd);
       $result = exec($cmd);
-      $this->debug->log("AuthController::save() exec_result:".print_r($result, true));
       $this->set('Title', 'User Save Error');
     } catch (Exception $e) {
       $this->debug->log("AuthController::create() error:" . $e->getMessage());
--- apps/models/UserModel.php
+++ apps/models/UserModel.php
@@ -38,7 +38,6 @@ class UserModel extends BaseModel {
   }
 
   public function auth($form) {
-    $this->debug->log("UserModel::auth() form:".print_r($form, true));
     $form[$this->model_name]['password'] = md5($form[$this->model_name]['password'].SALT);
     $data = $this->where('User.username', '=', $form[$this->model_name]['username'])
                  ->where('User.password', '=', $form[$this->model_name]['password'])
